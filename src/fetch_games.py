4b471cc819e400daeb01344f90f40017930e10e0
Sertalp B. Cay <sertalpbilal@gmail.com>
2023-04-05 23:49:53 -0400

    Initial commit

---
+++ src/fetch_games.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import pandas as pd
+import os
+import glob
+import time
+
+PREM_URL = r"https://www.fotmob.com/api/leagues?id=47"
+GAME_URL = r"https://www.fotmob.com/api/matchDetails?matchId={match_id}"
+
+def fetch_game_ids():
+    r = requests.get(PREM_URL)
+    games = r.json()['matches']['allMatches']
+    return games
+
+def download_game_data(game):
+    game_id = game['id']
+    file_name = f"../data/{game_id}.json"
+    if os.path.exists(file_name):
+        print(f"Game {game_id} data exists, skipping...")
+        return
+    r = requests.get(GAME_URL.format(match_id=game_id))
+    if r.status_code == 200:
+        json_data = r.json()
+        if not json_data['general']['started'] or not json_data['general']['finished']:
+            print(f"Game {game_id} is not finished, skipping...")
+            return
+        with open(file_name, "w") as f:
+            json.dump(json_data, f)
+            print(f"Game {game_id} is downloaded!")
+        time.sleep(0.5)
+
+def generate_index():
+    game_files = glob.glob("../data/*.json")
+    games = []
+    for g in game_files:
+        with open(g) as f:
+            d = json.load(f)
+        games.append(d['general'])
+    with open("../index.json", "w") as f:
+        json.dump(games, f)
+
+if __name__ == "__main__":
+    games = fetch_game_ids()
+    print(f"Total number of games: {len(games)}")
+    # for g in games:
+    #     download_game_data(g)
+    generate_index()
+
---
+++ static/js/main.js
@@ -0,0 +1,43 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        game_index: undefined
+    },
+    computed: {
+        reverse_ordered() {
+            if (_.isEmpty(this.game_index)) { return []}
+            return this.game_index.sort((a,b) => {
+                return (new Date(b.matchTimeUTCDate)) - (new Date(a.matchTimeUTCDate))
+            })
+        }
+    },
+    methods: {
+        open_game_page(e) {
+            let tm = e.currentTarget.dataset.href
+            window.location = tm
+        }
+    }
+})
+
+function read_local_file(url) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: url,
+            async: true,
+            success: function(data) {
+                resolve(data);
+            },
+            error: function() {
+                reject("No data");
+            }
+        });
+    });
+}
+
+$(document).ready(() => {
+    read_local_file("index.json").then((d) => {
+        app.game_index = d
+    })
+})
